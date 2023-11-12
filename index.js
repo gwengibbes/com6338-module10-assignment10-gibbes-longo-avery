fbc830011c8fafba93f7e7c21caad2118a091dc5
“Elodie <mail.elodielongo@gmail.com>
2023-11-12 14:47:16 +0100

    APIs keys and URLs

---
+++ index.js
@@ -0,0 +1,17 @@
+/* Spoonacular API 
+
+const SpoonacularKey = "*****************************";
+
+let FindRecipeUrl = "https://api.spoonacular.com/recipes/findByIngredients?ingredients=meat&number=3";
+
+Edamam API 
+Access Point: https://api.edamam.com/api/recipes/v2
+
+Application ID:
+22b25df1
+
+This is the application ID, you should send with each API request.
+Application Keys
+fedc606a902ac509ffa30b75cad44f35 —
+`
+*/
