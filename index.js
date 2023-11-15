738cc877ce3d7ba856dfeb16b6aa7ff0c890ed5d
ElodieLongo <133866010+ElodieLongo@users.noreply.github.com>
2023-11-15 19:35:40 +0100

    Add search input in addition to dropdown suggestions based on our archive
    Resolves issue #27

--- index.js
+++ index.js
@@ -5,45 +5,18 @@
 
 const lsWinePrefKey = 'sml.winePref'
 const edamamKey="fedc606a902ac509ffa30b75cad44f35";
-const spoonacularKey="0a62124c70384954a9d43515e0216eb5";
 //get wineFamily from localStorage
 const wineFamily= getWineFamilyPreference(); // or"white" depending on user choice from homepage 
 
-const redWines= ["bonarda", "bairrada", "primitivo", "nebbiolo", "dolcetto", "rioja", "grenache", "malbec", "zinfandel", "carmenere", "cesanese", "aglianico", "tempranillo", "shiraz", "merlot", "bordeaux"];
-const whiteWines= ["verdicchio", "chardonnay", "frascati", "gavi", "trebbiano", "catarratto", "albarino", "arneis", "verdejo", "vermentino", "soave", "grechetto", "riesling", "sauternes", "sylvaner"];
+// fix: added by me
+window.lsWinePrefKey = lsWinePrefKey;
+// putting variables in window object is considered a BAD PRACTICE! 
+// we should create an utility file to import function from the files
+window.getWineFamilyPreference = getWineFamilyPreference;
 
 function getWineFamilyPreference() {
     // get wine family from localStorage
     return localStorage.getItem(lsWinePrefKey);
-}
-    
-document.addEventListener("DOMContentLoaded", async (event) => { 
-    /* const wines= await fetchWines(wineFamily)
-    console.log(wines)  */
-});
-
-async function fetchWines(wineFamily){  
-
-    const recommendedWines = await fetch(`https://api.spoonacular.com/food/wine/description?apiKey=${spoonacularKey}&wine=merlot&number=2`);
-    // in above line I will need to replace merlot with the selection from the user in our search menu
-    return await recommendedWines.json();
-}
-
-function createSelectOptions(){
-    const selectElement = document.getElementById("wine-select")
-    let winesArray;
-    if(wineFamily === "white"){
-        winesArray = whiteWines;  
-    } else {
-        winesArray = redWines;  
-    }
-    winesArray.sort().forEach(wine =>{
-        const option = document.createElement("option");
-        option.value = wine;
-        option.text = wine;
-        selectElement.appendChild(option)
-    })
-    console.log(selectElement)
 }
 
 // Call the API to get the list of food pairings based on the selected wine
--- js/wine-pairing.js
+++ js/wine-pairing.js
@@ -1,16 +1,24 @@
 /*
     This is specifically for the wine pairing page
 */
+const spoonacularKey="0a62124c70384954a9d43515e0216eb5";
+const redWines= ["bonarda", "bairrada", "primitivo", "nebbiolo", "dolcetto", "rioja", "grenache", "malbec", "zinfandel", "carmenere", "cesanese", "aglianico", "tempranillo", "shiraz", "merlot", "bordeaux"];
+const whiteWines= ["verdicchio", "chardonnay", "frascati", "gavi", "trebbiano", "catarratto", "albarino", "arneis", "verdejo", "vermentino", "soave", "grechetto", "riesling", "sauternes", "sylvaner"];
+let selectElement;
 document.addEventListener("DOMContentLoaded", async (event) => {
-    const wineFamily = getWineFamilyPreference();
+    let wineFamily = window.getWineFamilyPreference();
 
     if(!wineFamily) {
+        // this has to be a redirect to home instead of an alert
         alert(`No wine selection has been made as yet`);
-        return;
+        wineFamily = "white";
     }
-    createSelectOptions();  
-    const wines= await fetchWines(wineFamily)
-    console.log(wines)
+
+    selectElement = document.getElementById("wine-select")
+    createSelectOptions()
+    selectElement.addEventListener("change", (event) => {
+        fetchWines(selectElement.value);
+    })
 
     // Populate the drop downlist with the list of wines from the API 
     /* 
@@ -20,3 +28,40 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     */
 });
 
+
+
+// this fetch function has to run on select change (once that user chooses a specific wine)
+// selectEl.addEventListener("change", (event) => {});
+async function fetchWines(wineFamily){  
+    const recommendedWines = await fetch(`https://api.spoonacular.com/food/wine/dishes?wine=${wineFamily}&apiKey=${spoonacularKey}`);
+    const wines = await recommendedWines.json();
+    const elementToAppend = document.getElementById('text-content');
+    const paragraph = document.createElement('p');
+    paragraph.textContent = wines.text;
+    const listContainer = document.createElement('ul');
+    wines.pairings?.forEach(el => {
+        const listEl = document.createElement('li');
+        listEl.textContent = el;
+        listContainer.appendChild(listEl)
+    })
+    
+    elementToAppend.appendChild(paragraph);
+    elementToAppend.appendChild(listContainer);
+    console.log(wines)
+}
+
+function createSelectOptions(){
+
+    let winesArray;
+    if(wineFamily === "white"){
+        winesArray = whiteWines;  
+    } else {
+        winesArray = redWines;  
+    }
+    winesArray.sort().forEach(wine =>{
+        const option = document.createElement("option");
+        option.value = wine;
+        option.text = wine;
+        selectElement.appendChild(option)
+    })
+}
